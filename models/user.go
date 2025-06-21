17c18d61e475ecd785247c51197c2d9d20bfe3c6
Gazal <gtech5277@gmail.com>
2025-06-21 23:50:15 +0530

    deleted

--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type user struct {
-	FirstName string `bson:"first_name" json:"first_name"`
-	LastName  string `bson:"last_name" json:"last_name"`
-	Email     string `bson:"email" json:"email"`
-	Phone     string `bson:"phone" json:"phone"`
-}
