d62a32320b930ad689beaab529540499a772a46a
Parth-11 <parth12.mgg@gmail.com>
2025-06-21 23:45:40 +0530

    Added user route

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/Parth-11/Athelia-Backend/db"
-	"github.com/Parth-11/Athelia-Backend/handler"
+	"github.com/Parth-11/Athelia-Backend/routes"
 	"gofr.dev/pkg/gofr"
 )
 
@@ -15,7 +15,7 @@ func main() {
 
 	app.AddMongo(mongoClient)
 
-	app.GET("/mongo", handler.Get_user)
+	routes.Setup_user_routes(app)
 
 	app.Run()
 }
--- routes/user.go
+++ routes/user.go
@@ -1,1 +1,7 @@
 package routes
+
+import "gofr.dev/pkg/gofr"
+
+func Setup_user_routes(app *gofr.App) {
+
+}
