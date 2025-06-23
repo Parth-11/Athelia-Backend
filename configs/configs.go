6480d5a9ba2ea86aa75df126308caf10a776c7ec
Parth-11 <parth12.mgg@gmail.com>
2025-06-23 23:06:41 +0530

    added value fetch from .env to configs.go

--- configs/configs.go
+++ configs/configs.go
@@ -1,6 +1,8 @@
 package configs
 
 import (
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -13,9 +15,40 @@ type Config struct {
 
 func NewConfig() *Config {
 	return &Config{
-		MongoURI:    "mongodb://localhost:27017",
-		Database:    "test",
-		Timeout:     4 * time.Second,
-		MaxPoolSize: 20,
+		MongoURI:    getEnv("MONGO_URI", "mongodb://localhost:27017"),
+		Database:    getEnv("MONGO_DB", "test"),
+		Timeout:     getEnvAsDuration("MONGO_TIMEOUT", 4*time.Second),
+		MaxPoolSize: getEnvAsUint64("MONGO_POOL_SIZE", 20),
 	}
+}
+
+func getEnv(key string, defaultVal string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	return defaultVal
+}
+
+func getEnvAsDuration(key string, defaultValue time.Duration) time.Duration {
+	if value := os.Getenv(key); value != "" {
+		d, err := time.ParseDuration(value)
+		if err == nil {
+			return d
+		}
+	}
+
+	return defaultValue
+}
+
+func getEnvAsUint64(key string, defaultValue uint64) uint64 {
+	if value := os.Getenv(key); value != "" {
+		parsed, err := strconv.ParseUint(value, 10, 64)
+
+		if err == nil {
+			return parsed
+		}
+	}
+
+	return defaultValue
 }
--- routes/user.go
+++ routes/user.go
@@ -1,7 +1,10 @@
 package routes
 
-import "gofr.dev/pkg/gofr"
+import (
+	"github.com/Parth-11/Athelia-Backend/handler"
+	"gofr.dev/pkg/gofr"
+)
 
 func Setup_user_routes(app *gofr.App) {
-
+	app.GET("/user{id}", handler.Get_user)
 }
